da9ee270bf6ea538f7d93a27f2246815a71d0b2b
Nguyen Vu Hoang Anh <nguyenvuhoanganh1309@gmail.com>
2026-04-22 20:44:54 +0700

    Configure CORS to allow localhost:3000

--- server.js
+++ server.js
@@ -11,7 +11,9 @@ const app = express();
 // =====================================================
 // MIDDLEWARE
 // =====================================================
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000',//check port
+}));
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
